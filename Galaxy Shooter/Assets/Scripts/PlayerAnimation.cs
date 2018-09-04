44b1524a0a048b56f0dca468cc67b64ef7434343
winterMute84 <wcarbajal84@gmail.com>
2018-09-03 21:58:25 -0700

    Implemented Post Processing Effects

--- Galaxy Shooter/Assets/Scripts/PlayerAnimation.cs
+++ Galaxy Shooter/Assets/Scripts/PlayerAnimation.cs
@@ -5,31 +5,31 @@ using UnityEngine;
 public class PlayerAnimation : MonoBehaviour
 {
 
-    private Animator _anim;
+	private Animator _anim;
 	// Use this for initialization
 	void Start ()
 	{
-	    _anim = GetComponent<Animator>();
+		_anim = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-	    if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
-	    {
-            _anim.SetBool("Turn_Left", true);
-            _anim.SetBool("Turn_Right", false);
-	    }
-        else if(Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.LeftArrow))
-            _anim.SetBool("Turn_Left", false);
+		if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+		{
+			_anim.SetBool("Turn_Left", true);
+			_anim.SetBool("Turn_Right", false);
+		}
+		else if(Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.LeftArrow))
+			_anim.SetBool("Turn_Left", false);
 
-	    if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
-	    {
-            _anim.SetBool("Turn_Right", true);
-            _anim.SetBool("Turn_Left", false);
-	    }
-        else if(Input.GetKeyUp(KeyCode.D) || Input.GetKeyUp(KeyCode.RightArrow))
-            _anim.SetBool("Turn_Right", false);
+		if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+		{
+			_anim.SetBool("Turn_Right", true);
+			_anim.SetBool("Turn_Left", false);
+		}
+		else if(Input.GetKeyUp(KeyCode.D) || Input.GetKeyUp(KeyCode.RightArrow))
+			_anim.SetBool("Turn_Right", false);
 
 
 	}
